7f8539584d994310b0dec335c7193625f01a1fe2
Slobberbone <slobberbone4884@gmail.com>
2018-03-12 09:54:02 +0100

    Ajout d'un timeout de test de connexion dans le service

--- 3rdparty/adbkeepconnection.sh
+++ 3rdparty/adbkeepconnection.sh
@@ -1,12 +1,15 @@
+
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-  sudo_prefix=sudo;
+if [[ $EUID -ne 0 ]];
+then
+  sudo_prefix=sudo
 fi
-result=$sudo_prefix adb devices | grep $1 | cut -f2 | xargs
+
 while sleep $2
 do
-if [ $result -neq 'device' ]
+result=$sudo_prefix adb devices | grep $1 | cut -f2 | xargs
+if [[ $result -ne 'device' ]]
 then
   $sudo_prefix adb connect $1
 fi
